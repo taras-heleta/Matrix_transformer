6cb2b9c7496afba64110c43c975b9f1c69fd70ad
Taras Heleta <fry.farnsworth74@gmail.com>
2019-04-15 14:34:56 +0300

    Add methods to RandomMatrixGenerator

--- spec/random_matrix_generator.rb
+++ spec/random_matrix_generator.rb
@@ -1,21 +1,49 @@
 module RandomMatrixGenerator
   class << self
     def square_matrix
-      size = rand(max_size)
-      matrix(size)
+      matrix(random_size)
     end
 
     def rectangular_matrix
-      rows = rand(max_size)
+      rows = random_size
       matrix(rows, rows + 1)
+    end
+
+    def one_element_matrix
+      matrix(random_number)
+    end
+
+    def one_dimensional_matrix
+      [random_number]
+    end
+
+    def not_rectangular_matrix
+      array = random_array
+      longer_array = array.clone.push(random_number)
+      [
+          array,
+          longer_array
+      ]
     end
 
     private
 
     def matrix(rows, columns = rows)
       Array.new(rows) do
-        Array.new(columns) {rand(values_range)}
+        random_array(columns)
       end
+    end
+
+    def random_array(number_of_elements = 5)
+      Array.new(number_of_elements) {random_number}
+    end
+
+    def random_number
+      rand(values_range)
+    end
+
+    def random_size
+      rand(max_size)
     end
 
     def max_size
